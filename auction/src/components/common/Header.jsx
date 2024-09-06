a4e680c08616c6db3bc8cbc70c30694e8713c9ea
Israt-Zahan <isratera942@gmail.com>
2024-09-07 01:46:42 +0600

    upload

---
+++ auction/src/components/common/Header.jsx
@@ -0,0 +1,80 @@
+import React from "react";
+import { Link, useNavigate } from "react-router-dom";
+
+const Header = () => {
+  const navigate = useNavigate();
+
+  function checkUserLoggedIn() {
+    const token = localStorage.getItem("token");
+    return !!token; // Returns true if token exists, false otherwise
+  }
+
+  const userLoggedIn = checkUserLoggedIn();
+
+  const handleLogout = () => {
+    localStorage.clear();
+    navigate("/home");
+  };
+
+  return (
+    <nav className="bg-gray-800">
+      <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+        <div className="flex items-center md:justify-between justify-center md:gap-0 gap-1 h-16">
+          <div className="flex items-center">
+            <div className="flex-shrink-0">
+            </div>
+            {/* Links */}
+            <div className="flex flex-row gap-0">
+              <Link
+                to="/"
+                className="text-white rounded-md px-1 py-2 text-sm font-medium"
+                aria-current="page"
+              >
+                Home
+              </Link>
+              <Link
+                to="/admin"
+                className="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-1 py-2 text-sm font-medium"
+              >
+                Admin
+              </Link>
+              <Link
+                to="/customer"
+                className="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-1 py-2 text-sm font-medium"
+              >
+                Customer
+              </Link>
+            </div>
+          </div>
+          <div className="flex items-center flex-row ">
+            {!userLoggedIn ? (
+              <Link
+                to="/login"
+                className="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium"
+              >
+                Login
+              </Link>
+            ) : (
+              <div className="flex flex-row gap-2">
+                <Link
+                  onClick={handleLogout}
+                  className="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium"
+                >
+                  Logout
+                </Link>
+                {/* <Link
+                  to="/changepass"
+                  className="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium"
+                >
+                  Change Password
+                </Link> */}
+              </div>
+            )}
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Header;
---
+++ backend/routes/auctionRoute.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const router = express.Router();
+const {
+  createAuction,
+  getAllAuctions,
+  getAuctionById,
+  updateAuction,
+  deleteAuction,
+  bidAuctionbyId,
+  bidpayId,
+  bidpayStripe,
+} = require("../controller/auctionController");
+
+// create auction
+router.post("/createauction", createAuction);
+
+// get all auctions
+router.get("/getallauctoin", getAllAuctions);
+
+// get a specific auction by ID
+router.get("/:email", getAuctionById);
+
+// update an auction by ID
+router.put("/:id", updateAuction);
+
+router.put("/bid/:id", bidAuctionbyId);
+
+router.put("/bid/pay/:id", bidpayId);
+
+router.post("/bid/pay/stripe/:id",bidpayStripe)
+
+// delete an auction by ID
+router.delete("/:id", deleteAuction);
+
+module.exports = router;
